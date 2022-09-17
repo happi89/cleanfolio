9a7e1429bae7d8751e973a1895630409ea80ab2a
happi89 <farhaanp99@gmail.com>
2022-09-17 14:07:20 -0400

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://farhaan-patel.vercel.app',
+  homepage: 'https://happi89.github.io',
   title: 'HP.',
 }
 
